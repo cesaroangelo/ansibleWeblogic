f29886b52e4a2baaa50e3e2f0ca5917e11525872
angelo <angelo@thinkx230.loc>
2015-02-17 23:59:28 +0100

    added missed files

---
+++ files/etc_profile.d_java.sh
@@ -0,0 +1,10 @@
+JAVA_VENDOR=Sun
+JAVA_HOME=/usr/java/latest
+PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
+JRE_HOME=/usr/java/latest/jre
+PATH=$PATH:$HOME/bin:$JRE_HOME/bin
+export JAVA_VENDOR
+export JAVA_HOME
+export JRE_HOME
+export PATH
+
---
+++ files/silentWeblogic.sh
@@ -0,0 +1,16 @@
+WL_HOME=/opt/oracle/weblogic/wlserver_10.3
+DOMAIN_HOME=/opt/oracle/weblogic/user_projects/domains/SitesDomain
+PRE_CLASSPATH=/opt/oracle/weblogic/user_projects/domains/CS/WEB-INF/lib/commons-lang-2.4.jar
+
+# 20140704 update classpath env.var
+# CLASSPATH=/opt/oracle/weblogic/patch_wls1036/profiles/default/sys_manifest_classpath/weblogic_patch.jar:/opt/oracle/weblogic/patch_ocp371/profiles/default/sys_manifest_classpath/weblogic_patch.jar:/usr/java/latest/lib/tools.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/weblogic_sp.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/weblogic.jar:/opt/oracle/weblogic/modules/features/weblogic.server.modules_10.3.6.0.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/webservices.jar:/opt/oracle/weblogic/modules/org.apache.ant_1.7.1/lib/ant-all.jar:/opt/oracle/weblogic/modules/net.sf.antcontrib_1.1.0.0_1-0b2/lib/ant-contrib.jar
+
+CLASSPATH=/usr/java/latest/lib/tools.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/weblogic_sp.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/weblogic.jar:/opt/oracle/weblogic/modules/features/weblogic.server.modules_10.3.6.0.jar:/opt/oracle/weblogic/wlserver_10.3/server/lib/webservices.jar:/opt/oracle/weblogic/modules/org.apache.ant_1.7.1/lib/ant-all.jar:/opt/oracle/weblogic/modules/net.sf.antcontrib_1.1.0.0_1-0b2/lib/ant-contrib.jar
+
+
+PATH=/opt/oracle/weblogic/wlserver_10.3/server/bin:/opt/oracle/weblogic/modules/org.apache.ant_1.7.1/bin:/usr/java/latest/jre/bin:/usr/java/latest/bin:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/weblogic/bin:/usr/java/latest/bin:/home/weblogic/bin:/usr/java/latest/jre/bin:/home/weblogic/bin
+export WL_HOME
+export DOMAIN_HOME
+export PRE_CLASSPATH
+export CLASSPATH
+export PATH
