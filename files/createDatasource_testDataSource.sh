b95cfbaeea726ae9cd1f0ac7612f1b512207af0c
angelo <angelo@thinkx230.loc>
2015-03-02 00:01:51 +0100

    fixed RHEL 6.x problems

--- files/createDatasource_testDataSource.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java weblogic.Admin -username weblogic -password weblogic -url t3://localhost:7001 TEST_POOL csDataSource
--- files/oracleDBCreateUser.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER ORACLEUSER IDENTIFIED BY ORACLEPASS;
-GRANT CONNECT, RESOURCE to ORACLEUSER;
-CREATE TABLESPACE CMS_DATA datafile '/u01/app/oracle/oradata/XE/cms_data.dbf' size 32m autoextend on;
-ALTER USER ORACLEUSER default tablespace CMS_DATA;
-exit
-
